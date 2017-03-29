1af4e5c9205ef599a1a760c477170b34abae46a7
Adrien BESSY AB250579 <adrien.bessy@cea.fr>
2017-03-29 11:23:11 +0200

    some corrections to connect interdistances_functions.py to the other python files

--- get_conserved_LFY_bs_list_part2.py
+++ get_conserved_LFY_bs_list_part2.py
@@ -11,7 +11,8 @@ This program was written by Adrien Bessy, Arnaud Stigliani and Francois Parcy, a
 and (4-scores.py and CalculateScoreAboveTh_and_Interdistances.py) programs written by Laura Gregoire.
 '''
 
-import interdistances_functions
+import re
+from interdistances_functions import *
 import ast
 import numpy as np
 from Bio import SeqIO
@@ -37,22 +38,7 @@ threshold = args.threshold
 if factorTranscription == "LFY_scores_matrix_19nucl" :
 	MatrixFile = "LFY_scores_matrix_19nucl.txt" 
 	dependencyFile = "interdependent_bases_matrix_for_LFY.txt"
-
-####################### This is useful to calculate interdependances ###########
-
-codigo = { 'ACC': 5, 'ATG': 14, 'AAG': 2, 'AAA': 0, 'ATC': 13, 'AAC': 1, 'ATA': 12,
-           'AGG': 10, 'CCT': 23, 'ACT': 7, 'AGC': 9, 'ACA': 4, 'AGA': 8, 'CAT': 19, 
-           'AAT': 3, 'ATT': 15, 'CTG': 30, 'CTA': 28, 'CTC': 29, 'CAC': 17, 'ACG': 6,
-           'CAA': 16, 'AGT': 11, 'CCA': 20, 'CCG': 22, 'CCC': 21, 'TAT': 51, 'GGT': 43, 
-           'TGT': 59, 'CGA': 24, 'CAG': 18, 'CGC': 25, 'GAT': 35, 'CGG': 26, 'CTT': 31, 
-           'TGC': 57, 'GGG': 42, 'TAG': 50, 'GGA': 40, 'TAA': 48, 'GGC': 41, 'TAC': 49,
-           'GAG': 34, 'TCG': 54, 'TTA': 60, 'GAC': 33, 'CGT': 27, 'TTT': 63,
-           'TCA': 52, 'GCA': 36, 'GTA': 44, 'GCC': 37, 'GTC': 45, 'GCG': 38,
-           'GTG': 46, 'TTC': 61, 'GTT': 47, 'GCT': 39, 'TGA': 56, 'TTG': 62,
-           'TCC': 53, 'TGG': 58, 'GAA': 32, 'TCT': 55}
-           
-codigoi = { "A" : "T", "C" : "G", "G" : "C", "T" : "A"}
-##################################
+	matrixType = "score"
 
 # These 3 lines allows to retrieve the matrix from the file
 F = open(MatrixFile,"r")
@@ -60,10 +46,10 @@ matrix = F.read()
 F.close()
 
 # These 3 lines allows to retrieve all the individual frequency values (retrieve only floats) from the matrix and put them in order into a list
-import re
+
 num = re.compile(r"([+-]?\d+[.,]\d+)")
 Mdata = num.findall(matrix)
-matScore, lenMotif = get_score_matrix(Mdata)
+matScore, lenMotif = get_score_matrix(Mdata,matrixType)
 
 '''The following line allows to produce the reversed matrix
 if we take the example given before : A T G C
@@ -79,7 +65,7 @@ dependency_matrix = [[4, 5, 6], [-2.482, -2.8048, -5.8493, -1.9992, -4.463, -5.8
 			[9, 10, 11], [-4.3503, -5.1942, -4.3503, -5.1942, -5.1942, -5.1942, -3.434, -5.1942, -3.9448, ...], 
 			[14, 15, 16], [-6.0225, -6.0225, -6.0225, -6.0225, -1.1954, -5.8493, -5.8493, -5.8493, -6.0225, -6.0225, -6.0225, ...]]
 '''
-dependency_matrix = get_dependency_matrix(dependencyFile)
+dependency_matrix = get_dependency_matrix(dependencyFile,num)
 
 ############### new part###########################
 with open('list_of_the_LFY_binding_sites.txt', 'r') as f:
